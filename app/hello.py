5d715339a3c54a6d9eb0c382a97984a457026121
anton <anton.tutoveanu@gmail.com>
2019-03-25 16:03:46 +1100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Roadside Assistance Application"
+    return "Roadside Assistance Application"
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
